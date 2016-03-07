c98f7d2b38dd1627c5cbddfb67ad84e8951f86ff
gyod <gyomail@gmail.com>
2016-03-07 23:55:01 +0900

    Sync Rev.1311
    Signed-off-by: gyod <gyomail@gmail.com>

--- servers/dist/game/data/scripts/quests/QuestMasterHandler.java
+++ servers/dist/game/data/scripts/quests/QuestMasterHandler.java
@@ -187,6 +187,7 @@ import quests.Q00904_DragonTrophyAntharas.Q00904_DragonTrophyAntharas;
 import quests.Q00905_RefinedDragonBlood.Q00905_RefinedDragonBlood;
 import quests.Q00906_TheCallOfValakas.Q00906_TheCallOfValakas;
 import quests.Q00907_DragonTrophyValakas.Q00907_DragonTrophyValakas;
+import quests.Q00943_FillingTheEnergyOfDestruction.Q00943_FillingTheEnergyOfDestruction;
 import quests.Q00998_FallenAngelSelect.Q00998_FallenAngelSelect;
 import quests.Q10273_GoodDayToFly.Q10273_GoodDayToFly;
 import quests.Q10274_CollectingInTheAir.Q10274_CollectingInTheAir;
@@ -289,6 +290,7 @@ import quests.Q10762_MarionetteSpirit.Q10762_MarionetteSpirit;
 import quests.Q10763_TerrifyingChertuba.Q10763_TerrifyingChertuba;
 import quests.Q10764_FreeSpirit.Q10764_FreeSpirit;
 import quests.Q10769_LettersFromTheQueen_CrumaTower.Q10769_LettersFromTheQueen_CrumaTower;
+import quests.Q10770_InSearchOfTheGrail.Q10770_InSearchOfTheGrail;
 import quests.Q10782_LettersFromTheQueen_ForsakenPlains.Q10782_LettersFromTheQueen_ForsakenPlains;
 import quests.Q10785_LettersFromTheQueen_FieldsOfMassacre.Q10785_LettersFromTheQueen_FieldsOfMassacre;
 import quests.Q10789_LettersFromTheQueen_SwampOfScreams.Q10789_LettersFromTheQueen_SwampOfScreams;
@@ -471,6 +473,7 @@ final class QuestMasterHandler
 		Q00905_RefinedDragonBlood.class,
 		Q00906_TheCallOfValakas.class,
 		Q00907_DragonTrophyValakas.class,
+		Q00943_FillingTheEnergyOfDestruction.class,
 		Q00998_FallenAngelSelect.class,
 		Q10273_GoodDayToFly.class,
 		Q10274_CollectingInTheAir.class,
@@ -573,6 +576,7 @@ final class QuestMasterHandler
 		Q10763_TerrifyingChertuba.class,
 		Q10764_FreeSpirit.class,
 		Q10769_LettersFromTheQueen_CrumaTower.class,
+		Q10770_InSearchOfTheGrail.class,
 		Q10782_LettersFromTheQueen_ForsakenPlains.class,
 		Q10785_LettersFromTheQueen_FieldsOfMassacre.class,
 		Q10789_LettersFromTheQueen_SwampOfScreams.class,
